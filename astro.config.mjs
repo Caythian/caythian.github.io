cf61974bafff63cccfbb316752b7ba098e7c748b
Xuecong Wang <xcatherine020202@gmail.com>
2025-12-05 22:18:12 -0500

    Deploy with action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { rehypeImageCaptions } from './src/lib/rehype-image-captions.ts';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://caythian.github.io',
   integrations: [react()],
   markdown: {
     rehypePlugins: [rehypeImageCaptions],
